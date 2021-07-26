1b898222370af28ad160ea464edcd4226038d41e
KMyroslav <kukhtarukm@gmail.com>
2021-07-27 02:02:26 +0300

    Adds painting feature

--- src/index.js
+++ src/index.js
@@ -4,23 +4,43 @@ Any live cell with more than three live neighbours dies, as if by overcrowding.
 Any live cell with two or three live neighbours lives on to the next generation.
 Any dead cell with exactly three live neighbours becomes a live cell.
  */
-let start = 0;
-let continious;
+
+let continious,
+  curX,
+  curY,
+  start = 0;
 const button = document.querySelector('.btn');
 const buttonRst = document.querySelector('.btn-reset');
-const width =
-  document.querySelector('.canvas').clientWidth -
-  (document.querySelector('.canvas').clientWidth % 2);
-const height =
-  document.querySelector('.canvas').clientHeight -
-  (document.querySelector('.canvas').clientHeight % 2);
-const canvas = document.querySelector('.canvas').getContext('2d');
-const size = Math.round((width * height) / ((width * height) / 8));
-const cols = Math.round(width / size);
-const rows = Math.round(height / size);
+let canvas = document.querySelector('.canvas');
+let canvas2D = document.querySelector('.canvas').getContext('2d');
+const size = 8;
+const width = canvas.clientWidth - (canvas.clientWidth % size);
+const height = canvas.clientHeight - (canvas.clientHeight % size);
+const cols = width / size;
+const rows = height / size;
+canvas.setAttribute('style', `width:${width}px; height:${height}px`);
+canvas.setAttribute('width', `${width}`);
+canvas.setAttribute('height', `${height}`);
 
-document.querySelector('.canvas').setAttribute('width', `${width}`);
-document.querySelector('.canvas').setAttribute('height', `${height}`);
+canvas.onmousemove = function (e) {
+  curX = Math.floor(e.offsetX / size);
+  curY = Math.floor(e.offsetY / size);
+};
+
+canvas.onmousedown = function () {
+  arr[curX][curY][2] = arr[curX][curY][2] === 0 ? 1 : 0;
+  canvas2D.fillStyle = `rgb(
+          0,
+          0,
+          ${Math.floor(Math.random() * 255)})`;
+  if (arr[curX][curY][2] === 1) {
+    canvas2D.fillRect(curX * size, curY * size, size, size);
+  }
+  if (arr[curX][curY][2] === 0) {
+    canvas2D.clearRect(curX * size, curY * size, size, size);
+  }
+  console.log(arr[curX][curY][2]);
+};
 
 function countNeighbours(i, j, arr) {
   /*
@@ -101,39 +121,39 @@ function countNeighbours(i, j, arr) {
   }
 }
 
-const make2Darray = function () {
+function make2Darray() {
   const arr = new Array(cols);
   for (let i = 0; i < cols; i += 1) {
     arr[i] = new Array(rows);
   }
   for (let i = 0; i < cols; i += 1) {
     for (let j = 0; j < rows; j += 1) {
-      let z;
-      if (j % 2 === 0) {
-        z = Math.round(Math.random() / 1.95);
-      } else {
-        z = Math.round(Math.random() / 1.9);
-      }
+      let z = 0;
+      // if (j % 2 === 0) {
+      //   z = Math.round(Math.random() / 1.95);
+      // } else {
+      //   z = Math.round(Math.random() / 1.9);
+      // }
       arr[i][j] = [i, j, z];
     }
   }
   for (let i = 0; i < cols; i += 1) {
     for (let j = 0; j < rows; j += 1) {
       arr[i][j].push(countNeighbours(i, j, arr));
+      canvas2D.fillStyle = `rgb(
+          0,
+          0,
+          ${Math.floor(Math.random() * 255)})`;
       if (arr[i][j][2] === 1) {
-        canvas.fillStyle = `rgb(
-            0,
-            0,
-            ${Math.floor(Math.random() * 255)})`;
-        canvas.fillRect(i * size, j * size, size, size);
+        canvas2D.fillRect(i * size, j * size, size, size);
       }
       if (arr[i][j][2] === 0) {
-        canvas.clearRect(i * size, j * size, size, size);
+        canvas2D.clearRect(i * size, j * size, size, size);
       }
     }
   }
   return arr;
-};
+}
 
 let arr = make2Darray();
 
@@ -146,27 +166,31 @@ function gameOflife() {
         arr[i][j][2] = 0;
       } else if (neighbours > 3) {
         arr[i][j][2] = 0;
-      } else if ((arr[i][j][2] === 1 && neighbours === 2) || neighbours === 3) {
+      } else if (
+        (arr[i][j][2] === 1 && neighbours === 2) ||
+        (arr[i][j][2] === 1 && neighbours === 3)
+      ) {
         arr[i][j][2] = 1;
       } else if (arr[i][j][2] === 0 && neighbours === 3) {
         arr[i][j][2] = 1;
       }
 
       if (arr[i][j][2] === 1) {
-        canvas.fillStyle = `rgb(
+        canvas2D.fillStyle = `rgb(
             0,
             0,
             ${Math.floor(Math.random() * 255)})`;
-        canvas.fillRect(i * size, j * size, size, size);
+        canvas2D.fillRect(i * size, j * size, size, size);
       }
       if (arr[i][j][2] === 0) {
-        canvas.clearRect(i * size, j * size, size, size);
+        canvas2D.clearRect(i * size, j * size, size, size);
       }
     }
   }
 }
 
 button.addEventListener('click', onBtnClick);
+buttonRst.addEventListener('click', onRstBtnClick);
 
 function onBtnClick() {
   start = start === 0 ? 1 : 0;
@@ -178,8 +202,6 @@ function onBtnClick() {
     button.textContent = 'Start';
   }
 }
-
-buttonRst.addEventListener('click', onRstBtnClick);
 
 function onRstBtnClick() {
   arr = make2Darray();
